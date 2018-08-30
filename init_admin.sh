b961acfc94840dcace49dff3ac32c5dd161b8d5b
warking <warking.net@gmail.com>
2018-08-30 20:56:30 +0800

    Update init_admin.sh
    error fix

--- init_admin.sh
+++ init_admin.sh
@@ -1,7 +1,6 @@
 #!/bin/sh 
 
-
-docker exec taiga-back "cd /home/taiga/taiga-back &&  python manage.py loaddata initial_user"
-docker exec taiga-back "cd /home/taiga/taiga-back && python manage.py loaddata initial_project_templates"
+docker exec taiga-back python manage.py loaddata initial_user
+docker exec taiga-back python manage.py loaddata initial_project_templates
 
 exit 0
